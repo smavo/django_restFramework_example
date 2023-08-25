ce78a1e2e1595361726a56a0b257769700f58566
Sergio Villagomez Ortega <martin.v.ortega@gmail.com>
2023-08-24 22:29:35 -0500

    Add Review [Urls, Views, Serializer]

--- django_rest_example/watchlist_app/api/serializer.py
+++ django_rest_example/watchlist_app/api/serializer.py
@@ -1,9 +1,18 @@
 from rest_framework import serializers
-from watchlist_app.models import WatchList, StreamPlatform
+from watchlist_app.models import WatchList, StreamPlatform, Review
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    review_user = serializers.StringRelatedField(read_only=True)
+
+    class Meta:
+        model = Review
+        # exclude = ('watchlist',)
+        fields = "__all__"
 
 
 class WatchListSerializer(serializers.ModelSerializer):
-    # reviews = ReviewSerializer(many=True, read_only=True)
+    reviews = ReviewSerializer(many=True, read_only=True)
     platform = serializers.CharField(source='platform.name')
 
     class Meta:
--- django_rest_example/watchlist_app/api/views.py
+++ django_rest_example/watchlist_app/api/views.py
@@ -1,8 +1,8 @@
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from watchlist_app.models import WatchList, StreamPlatform
-from watchlist_app.api.serializer import WatchListSerializer, StreamPlatformSerializer
+from watchlist_app.models import WatchList, StreamPlatform, Review
+from watchlist_app.api.serializer import WatchListSerializer, StreamPlatformSerializer, ReviewSerializer
 
 
 class StreamPlatformAV(APIView):
@@ -84,4 +84,44 @@ class WatchDetailAV(APIView):
     def delete(self, request, pk):
         movie = WatchList.objects.get(pk=pk)
         movie.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class ReviewList(APIView):
+    def get(self, request):
+        review = Review.objects.all()
+        serializer = ReviewSerializer(review, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = ReviewSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
+
+
+class ReviewDetail(APIView):
+    def get(self, request, pk):
+        try:
+            review = Review.objects.get(pk=pk)
+        except WatchList.DoesNotExist:
+            return Response({'error': 'Not found'}, status=status.HTTP_404_NOT_FOUND)
+
+        serializer = ReviewSerializer(review)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        review = Review.objects.get(pk=pk)
+        serializer = ReviewSerializer(review, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        review = Review.objects.get(pk=pk)
+        review.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
